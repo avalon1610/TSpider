732b58ff6cc17b5c5af6839772f66c1e8d01e0e9
root <root@ubuntu.(none)>
2013-08-09 02:05:15 -0700

    add pages

--- DHT/index.php
+++ DHT/index.php
@@ -5,6 +5,7 @@ header("Content-Type: text/html; charset=utf-8");
 
 //注意文件的编码格式需要保存为为UTF-8格式
 require ( "sphinxapi.php" );
+require ( "page.php");
 
 $cl = new SphinxClient ();
 $cl->SetServer ( '127.0.0.1', 9312);
@@ -24,14 +25,20 @@ $cl->SetFilterRange('group_id2',6,8);
 */
 
 //取从头开始的前20条数据，0,20类似SQl语句的LIMIT 0,20
-$cl->SetLimits(0,20);
+//$cl->SetLimits(0,20);
 
 //在做索引时，没有进行 sql_attr_类型 设置的字段，可以作为“搜索字符串”，进行全文搜索
 
 $keyword = '';
+$page = 1;
 
 if ($_GET)
+{
     $keyword = $_GET['q'];
+	if (array_key_exists('page',$_GET))
+		$page = $_GET['page'];
+}
+
 
 //如果需要搜索指定全文字段的内容，可以使用扩展匹配模式：
 //$cl->SetMatchMode(SPH_MATCH_EXTENDED);
@@ -57,14 +64,79 @@ function queryContent(&$connection,$query_id)
     return mysql_fetch_array($result);
 }
 
-/*
-echo '<pre>';
-print_r($res['matches']);
-print_r($res);
-print_r($cl->GetLastError());
-print_r($cl->GetLastWarning());
-echo '</pre>';
-*/
+function isAjax()
+{
+	if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])=='xmlhttprequest')
+		return true;
+	else
+		return false;
+}
+
+function display($keyword,$page,$cl)
+{
+	$response = "";
+	if ($keyword)
+	{
+   		$res = $cl->Query ( $keyword, "*" );    //"*"表示在所有索引里面同时搜索，"索引名称（例如test或者test,test2）"则表示搜索指定的
+    	$connection = NULL;
+		$response .= "<div id='Content'>";
+		if (!array_key_exists('matches',$res))
+			$response .= 'No Result.';
+		else
+		{
+			$response .= "<div id='result_count'>找到约".$res['total_found']."条结果（用时 ".$res['time']." 秒）</div>";
+    		foreach ($res['matches'] as $ids)
+			{
+	        	$row = queryContent($connection,$ids['id']);
+
+				$response .= <<<EOT
+				<div class='iP dropdown'>
+				<div class='mM'>
+					<div class='dF'>
+						<span role='button' class='sF' tabindex='0' title='{$row['Name']}'>
+							{$row['Name']}
+						</span>
+					</div>
+					<div class='tA'>
+						<div class='fK'>
+							<a href="#" class="button ddb"><span class="label">Files</span><span class="toggle"></span></a>
+							<a href="magnet:?xt=urn:btih:{$row['Hash']}" class="button" title="磁力链接"><span class="icon icon185"></span></a>
+						</div>
+					</div>
+				</div> <!--mM-->
+				<div class='Su'>
+					<div class="dropdown-slider">
+						{$row['Description']}
+					</div> <!-- /.dropdown-slider -->
+				</div><!--Su-->
+			</div><!--iP-->
+EOT;
+			}
+
+			// the pages
+			$response .= "<div class='page'>";
+			$params = array('total_rows'=>$res['total_found'],
+							'method'=>'ajax',
+							'ajax_func_name'=>'goToPage',
+							'now_page'=>$page
+					);
+			$page = new Core_Lib_Page($params);
+			$response .= $page->show(1);
+			$response .= "</div>";
+		}
+
+		$response .= "</div><!--class Content-->";
+	}
+	return $response;
+}
+
+//handle ajax request, changing page etc.
+if (isAjax())
+{
+	echo display($keyword,1,$cl);
+	return;
+}
+
 ?>
 <html>
 <head>
@@ -100,6 +172,7 @@ body {
 
 #Content {
 	width: 600px;
+	height: auto;
 	margin:10px 5px 10px 5px;
 }
 
@@ -120,7 +193,6 @@ body {
  	padding:10px 10px;
 	text-overflow:ellipsis;
 	display:block;
-	word-break:keep-all;
 	white-space:nowrap;
 }
 
@@ -152,9 +224,24 @@ body {
 	overflow:hidden;
 }
 
+dd {
+	margin-left:20px;
+	overflow:hidden;
+	text-overflow:ellipsis;
+	white-space:nowrap;
+}
+
+dd:hover {
+	white-space:normal;
+}
+
+.page {font:12px/16px arial;}
+.page span{float:left;margin:0px 3px;}
+.page a{float:left;margin:0 3px;border:1px solid #ddd;padding:3px 7px; text-decoration:none;color:#666}
+.page a.now_page,#page a:hover{color:#fff;background:#05c}
 
 </style>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>
 	$(document).ready(function() {
 		// Toggle the dropdown menu's
@@ -199,17 +286,22 @@ body {
 			var dd_slider = parent.find('.Su').find('.dropdown-slider');
 			dd_slider.slideToggle('fast');
 			
-			if (dd_slider.text().indexOf(' - ') == -1)
+			if (dd_slider.has('dl').length != 0)
 				return false;
+
 			var temp = '';
-			var content = dd_slider.text().split(' - ');
+			var content = dd_slider.text().split('\n');
 			dd_slider.text('');
 		
+			dd_slider.append('<dl>');
+			dl = dd_slider.find('dl');
 			for (var i in content)
 			{	
-				temp = '';
-				temp = '<p>' + content[i] + '</p>';
-				dd_slider.append(temp);
+				if (content[i].indexOf(' - ') == -1)
+					temp = '<dt>' + content[i] + '</dt>';
+				else
+					temp = '<dd>' + content[i] + '</dd>';
+				dl.append(temp);
 			}
 			
 			return false;
@@ -227,57 +319,29 @@ body {
 			$('span.toggle').removeClass('active');
 		}
 	});
+
+	function goToPage(page)
+	{
+		url = '?page='+page;
+		$.get(url,function(data){
+			$('.content').text('');
+			$('.content').append(data);
+		});
+	}
+
 </script>
- </head>
- <body>
+</head>
+<body>
  	<div id='search_bar'>
 		<form method='get'>
-			<input type='text' id='search_text' name='q' />
+			<input type='text' id='search_text' name='q' value='<?php echo $keyword;?>' />
 			<button class="action blue search_button"><span class="label">Search</span></button>
 		</form>
 	</div>
-	<div id='result_count'>找到约 98,800 条结果 （用时 0.29 秒） </div>
-	<div id='Content'>
 <?php
-if ($keyword)
-{
-    $res = $cl->Query ( $keyword, "*" );    //"*"表示在所有索引里面同时搜索，"索引名称（例如test或者test,test2）"则表示搜索指定的
-    $connection = NULL;
-	if (!array_key_exists('matches',$res))
-		echo 'No Result.';
-	else
-	{
-    	foreach ($res['matches'] as $ids)
-		{
-	        $row = queryContent($connection,$ids['id']);
-
-			echo <<<EOT
-			<div class='iP dropdown'>
-			<div class='mM'>
-				<div class='dF'>
-					<span role='button' class='sF' tabindex='0' title='{$row['Name']}'>
-						{$row['Name']}
-					</span>
-				</div>
-				<div class='tA'>
-					<div class='fK'>
-						<a href="#" class="button ddb"><span class="label">Files</span><span class="toggle"></span></a>
-						<a href="magnet:?xt=urn:btih:{$row['Hash']}" class="button" title="磁力链接"><span class="icon icon185"></span></a>
-					</div>
-				</div>
-			</div> <!--mM-->
-			<div class='Su'>
-				<div class="dropdown-slider">
-					{$row['Description']}
-				</div> <!-- /.dropdown-slider -->
-			</div><!--Su-->
-		</div><!--iP-->
-EOT;
-		}
-	}
-}
+	echo display($keyword,1,$cl);
 ?>
-	</div><!--class Content-->
+
 </body>
 </html>
 
